41e228a4bdfd7b59a4188b7a0b462e70965a63fc
Ivan de Queiroz <iqueiroz.go@gmail.com>
2018-09-13 23:13:45 -0300

    removing unused usings

--- Puzzles/OddOccurrencesInArray/Solution.cs
+++ Puzzles/OddOccurrencesInArray/Solution.cs
@@ -39,9 +39,6 @@ source: https://app.codility.com/programmers/lessons/2-arrays/odd_occurrences_in
 */
 namespace Puzzles.OddOccurrencesInArray
 {
-    using System;
-    using System.Linq;
-
     public class Solution
     {
         public int solution(int[] A)
