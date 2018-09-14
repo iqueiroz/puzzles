d40e553dc33a68d5c1d9b9251ebf23459b73917c
Ivan de Queiroz <iqueiroz.go@gmail.com>
2018-09-13 23:27:15 -0300

    Solution for FrogJmp

---
+++ Puzzles.Tests/FrogJmp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Puzzles.Tests
+{
+    public class FrogJmp
+    {
+        private Puzzles.FrogJmp.Solution _solution;
+
+        public FrogJmp()
+        {
+            this._solution = new Puzzles.FrogJmp.Solution();
+        }
+
+        [Theory]
+        [InlineData(10, 85, 30, 3)]
+        public void Examples(int x, int y, int d, int expectedResult)
+        {
+            var result = this._solution.solution(x, y, d);
+            Assert.Equal(expectedResult, result);
+        }
+
+    }
+}
